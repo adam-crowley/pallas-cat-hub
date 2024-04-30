ea47d6f0188e924a26c49b665a852c4ddfe1f458
Adam Crowley <adamcrowley@gmail.com>
2024-05-01 11:47:03 +1200

    Add content to Habitat component

--- src/components/Habitat.tsx
+++ src/components/Habitat.tsx
@@ -1,7 +1,26 @@
 function Habitat() {
   return (
     <>
-      <h1>Habitat</h1>
+      <div className="section--habitat">
+        <div className="container">
+          <div className="col-6 section--habitat__text">
+            <h2>Habitat</h2>
+            <p>
+              The Pallas's cat's range extends from the Caucasus eastward to
+              Central Asia, Mongolia and adjacent parts of Dzungaria and the
+              Tibetan Plateau. It inhabits montane shrublands and grasslands,
+              rocky outcrops, scree slopes and ravines in areas, where the
+              continuous snow cover is below 15–20 cm (6–8 in). In the
+              southwestern part of its range, the habitat of the Pallas's cat is
+              affected by cold and dry winters, and moderate to low rainfall in
+              warm summers.
+            </p>
+          </div>
+          <div className="col-6">
+            <img src="img/manul-distribution.png" alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }

945f029d1bee4086266fe67785e50085d6a4b843
Adam Crowley <adamcrowley@gmail.com>
2024-05-29 11:21:51 +1200

    Fix navigation bugs in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,24 @@ import { Link } from 'react-router-dom'
 import { HeaderProps } from '../../models/models'
 
 function Header({ scrollToSection, refs }: HeaderProps) {
-  const [isNavExpanded, setIsNavExpanded] = useState(true)
+  const [isExpanded, setIsExpanded] = useState(true)
+  const breakpoint = 768
+  let isMobile = window.innerWidth <= breakpoint
 
-  function handleClick() {
-    setIsNavExpanded(!isNavExpanded)
+  function toggleNav() {
+    isMobile ? setIsExpanded(!isExpanded) : setIsExpanded(false)
+  }
+
+  function closeNav() {
+    isMobile ? setIsExpanded(true) : null
+  }
+
+  function handleResize() {
+    isMobile = window.innerWidth <= breakpoint
+    setIsExpanded(isMobile)
   }
 
   useEffect(() => {
-    function handleResize() {
-      const breakpoint = 768
-      const isMobile = window.innerWidth <= breakpoint
-      setIsNavExpanded(isMobile)
-    }
     window.addEventListener('resize', handleResize)
     handleResize()
     return () => {
@@ -28,43 +34,71 @@ function Header({ scrollToSection, refs }: HeaderProps) {
       <header className="header">
         <Link
           to="/"
-          onClick={() => scrollToSection(refs.introRef)}
+          onClick={() => {
+            closeNav()
+            scrollToSection(refs.introRef)
+          }}
           className="header__logo"
         >
           <img src="/img/logo.png" alt="" />
         </Link>
         <h1 className="header__title">
-          <Link to="/">Pallas' Cat Hub</Link>
+          <Link
+            to="/"
+            onClick={() => {
+              closeNav()
+              scrollToSection(refs.introRef)
+            }}
+          >
+            Pallas' Cat Hub
+          </Link>
         </h1>
         <ul
           className={`header__nav ${
-            !isNavExpanded ? 'header__nav--open' : 'header__nav--closed'
+            !isExpanded ? 'header__nav--open' : 'header__nav--closed'
           }`}
         >
           <li>
-            <Link to="/facts" onClick={() => scrollToSection(refs.factsRef)}>
+            <Link
+              to="/facts"
+              onClick={() => {
+                scrollToSection(refs.factsRef)
+                toggleNav()
+              }}
+            >
               Facts
             </Link>
           </li>
           <li>
             <Link
               to="/habitat"
-              onClick={() => scrollToSection(refs.habitatRef)}
+              onClick={() => {
+                scrollToSection(refs.habitatRef)
+                toggleNav()
+              }}
             >
               Habitat
             </Link>
           </li>
           <li>
-            <Link to="/videos" onClick={() => scrollToSection(refs.videosRef)}>
+            <Link
+              to="/videos"
+              onClick={() => {
+                scrollToSection(refs.videosRef)
+                toggleNav()
+              }}
+            >
               Videos
             </Link>
           </li>
           <li>
-            <Link to="/database/japan">Database</Link>
+            <Link to="/database/japan" onClick={toggleNav}>
+              Database
+            </Link>
           </li>
         </ul>
-        <button className="header__nav-btn" onClick={handleClick}>
-          {isNavExpanded ? (
+        <button className="header__nav-btn" onClick={toggleNav}>
+          {isExpanded ? (
             <>
               <svg className="header__menu--open"></svg>
             </>
